986165fb1fc3030b06699c87c1e778e9442555bb
yanis_gougaud <tahlefifou@gmail.com>
2026-09-21 11:25:32 +0200

    Changement de mode 2D
    - Changement de mode 2D (top, coté)

--- engine2D/include/include.hpp
+++ engine2D/include/include.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include <SFML/Window/Keyboard.hpp>
 
 #include "components/collider/collider.hpp"
@@ -8,5 +9,4 @@
 #include "math/math.hpp"
 #include "math/vector2D.hpp"
 #include "components/renderer/renderer.hpp"
-
-
+#include "components/input/input.hpp"
